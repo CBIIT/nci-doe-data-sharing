e4f991eb3c5eab221bab3fa00bc44cca4cf94ffa
Mounica Ganta <mounica.ganta@gmail.com>
2020-04-21 18:52:12 -0400

    clean up redundant code.

--- src/main/resources/static/js/controller/search.js
+++ src/main/resources/static/js/controller/search.js
@@ -207,10 +207,7 @@ function renderPath(data, type, row) {
 			"<div class='col-md-12 top-buffer' style='margin-left:22px;'>" +
 			"<span class='cil_12_bold_no_color'>Program: </span><a class='cil_12_no_color button2a' metadata_type = '" + ins  + "' tabindex='0'" +
 			" data-container='body' data-toggle='popover' data-placement='right' data-trigger='click' data-popover-content='#a01'>" + row.programName + "</a>" +
-					"&nbsp&nbsp;<span class='editCollectionMetadata' metadata_path  = '" + row.institutePath+ "' metadata_set = '" + ins  + "'><i class='fa fa-edit' data-toggle='tooltip' data-content='Edit Institute Metadata'></i></span></div></div></div>" +
-			"<div class='row' style='margin-left: 25px;margin-bottom:10px;'><div class='col-sm-1 resultsStatsContainer' title='Number of Files'>" +
-			"<i class='fas fa-folder-open' title='Number of Files' style='font-size: 18px;'></i>" +
-			"<span class='resultsStatsText'> &nbsp;&nbsp;" + row.numOfDataSets + " </span></div></div>";
+					"&nbsp&nbsp;<span class='editCollectionMetadata' metadata_path  = '" + row.institutePath+ "' metadata_set = '" + ins  + "'><i class='fa fa-edit' data-toggle='tooltip' data-content='Edit Institute Metadata'></i></span></div></div></div>";
 
 	
     return html;
