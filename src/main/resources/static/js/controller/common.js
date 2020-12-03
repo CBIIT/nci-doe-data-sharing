654eb085f6446771aed5d61d3bdb9fdeb61fb4ca
Mounica Ganta <mounica.ganta@gmail.com>
2020-12-03 14:31:49 -0500

    MODAC-96

--- src/main/resources/static/js/controller/common.js
+++ src/main/resources/static/js/controller/common.js
@@ -202,7 +202,7 @@ $("#download-btn").click(function(e){
 	    d.endPointName = $("#endPointName").val();
 		d.endPointLocation = $("#endPointLocation").val();
 		d.drivePath = $("#drivePath").val();
-		//d.accessToken = $("#accessToken").val();
+		d.accessToken = $("#accessToken").val();
 		
 		var url;
 		if(selectedFiles) {
