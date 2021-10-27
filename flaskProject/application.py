5093236b58e0b1220f912559728e09abb8914f28
Mounica Ganta <mounica.ganta@gmail.com>
2021-10-27 13:38:39 -0400

    code clean up

--- flaskProject/application.py
+++ flaskProject/application.py
@@ -15,18 +15,21 @@ def modac_routing():
     datafilename = request.args.get("dataFileName", None)
     modelfilename = request.args.get("modelName", None)
     predictions_filename = request.args.get("resultFileName", None)
+    if datafilename is None:
+        datafilename = 'test_X.npy'
+    if modelfilename is None or not modelfilename:
+        modelfilename = 'mt_cnn_model.h5'
     if datafilename is not None:
         downloadFile(datafilename)
     if modelfilename is not None and modelfilename:
         print("model file here:" + modelfilename)
         downloadFile(modelfilename)
-    if datafilename is None:
-        datafilename = 'test_X.npy'
-    if modelfilename is None or not modelfilename:
-        modelfilename = 'mt_cnn_model.h5'
+
     print(predictions_filename)
     print(modelfilename)
-    main(predictions_filename, modelfilename, datafilename)
+    print(datafilename)
+    if predictions_filename is not None and modelfilename is not None and datafilename is not None:
+        main(predictions_filename, modelfilename, datafilename)
     # subprocess.call(['python', 'infer.py', predictions_filename, modelfilename, datafilename])
     # os.system("sbatch %s" % main)
     # os.system('ssh ncidoesvct2@batch.ncifcrf.gov python < infer.py >> mylog.txt 2>&1')
