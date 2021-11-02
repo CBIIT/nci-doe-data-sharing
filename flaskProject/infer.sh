8bab165160bb9d1b38418e8590e774e248220a45
Mounica Ganta <mounica.ganta@gmail.com>
2021-11-02 18:34:08 -0400

    MODAC-187;

--- flaskProject/infer.sh
+++ flaskProject/infer.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
-
+#!/bin/bash -l
+#SBATCH --job-name=infer
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=gantam2@nih.gov
+#SBATCH --account=ncidoesvct2
+#SBATCH --chdir=/home/ncidoesvct2
 echo "Starting infer script"
 echo datafilename is ${1}
 echo modelfilename is ${2}
 echo predfilename is ${3}
-source mt-cnn/bin/activate
+source /home/ncidoesvct2/mt-cnn/bin/activate
 python /home/ncidoesvct2/infer.py ${1} ${2} ${3}
 echo "Completed infer script"
-exit 0
