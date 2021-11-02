14868746e08a24c75de68366069750eb8df0f9de
Mounica Ganta <mounica.ganta@gmail.com>
2021-11-02 14:09:52 -0400

    MODAC-187;script and scheduler updates

--- flaskProject/infer.sh
+++ flaskProject/infer.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# This file is called ~/infer.sh
-#SBATCH --output=slurm.out     # file to collect standard output
-#SBATCH --error=slurm.err   # file to collect standard errors
-#SBATCH -o myfile.out
 
+echo "Starting infer script"
+echo datafilename is ${1}
+echo modelfilename is ${2}
+echo predfilename is ${3}
 source mt-cnn/bin/activate
-python /home/ncidoesvct2/infer.py %1 %2 %3
+python /home/ncidoesvct2/infer.py ${1} ${2} ${3}
+echo "Completed infer script"
+exit 0
