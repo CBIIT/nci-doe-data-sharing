50aecd5fb53076853d9151cdbe42baeea6844627
Mounica Ganta <mounica.ganta@gmail.com>
2022-01-25 17:39:51 -0500

    code clean up

--- src/main/java/gov/nih/nci/doe/web/scheduler/ManageTasksScheduler.java
+++ src/main/java/gov/nih/nci/doe/web/scheduler/ManageTasksScheduler.java
@@ -72,82 +72,83 @@ public class ManageTasksScheduler extends AbstractDoeController {
 	@Value("${gov.nih.nci.hpc.server.v2.bulkregistration}")
 	private String registrationServiceV2URL;
 
-//	@Scheduled(cron = "${doe.scheduler.cron.auditing}")
-//	public void updateAuditingService() throws DoeWebException {
-//
-//		String authToken = DoeClientUtil.getAuthenticationToken(writeAccessUserName, writeAccessUserPassword,
-//				authenticateURL);
-//
-//		String serviceURL = queryServiceURL + "?page=" + 1 + "&totalCount=true";
-//		HpcDownloadSummaryDTO downloads = DoeClientUtil.getDownloadSummary(authToken, serviceURL);
-//
-//		final MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
-//		paramsMap.set("totalCount", Boolean.TRUE.toString());
-//		HpcRegistrationSummaryDTO registrations = DoeClientUtil.getRegistrationSummary(authToken,
-//				registrationServiceV2URL, paramsMap);
-//
-//		List<HpcUserDownloadRequest> downloadResults = new ArrayList<HpcUserDownloadRequest>();
-//		if (downloads != null) {
-//			downloadResults.addAll(downloads.getActiveTasks());
-//			downloadResults.addAll(downloads.getCompletedTasks());
-//		}
-//
-//		List<HpcBulkDataObjectRegistrationTaskDTO> uploadResults = new ArrayList<HpcBulkDataObjectRegistrationTaskDTO>();
-//		if (registrations != null) {
-//			uploadResults.addAll(registrations.getActiveTasks());
-//			uploadResults.addAll(registrations.getCompletedTasks());
-//		}
-//
-//		List<Auditing> auditingTaskIds = auditingService.getAllTaskIds();
-//
-//		if (CollectionUtils.isNotEmpty(auditingTaskIds)) {
-//			for (Auditing audit : auditingTaskIds) {
-//				if (audit.getOperation().equalsIgnoreCase("Upload")) {
-//					HpcBulkDataObjectRegistrationTaskDTO upload = uploadResults.stream()
-//							.filter(x -> audit.getTaskId().equals(x.getTaskId())).findAny().orElse(null);
-//
-//					if (upload != null) {
-//						audit.setCompletionTime(
-//								(upload != null && upload.getCompleted() != null) ? upload.getCompleted().getTime()
-//										: null);
-//						if (upload.getResult() == null) {
-//							audit.setStatus("In progress");
-//						} else if (Boolean.TRUE.equals(upload.getResult())) {
-//							audit.setStatus("Completed");
-//						} else if (Boolean.FALSE.equals(upload.getResult())) {
-//							audit.setStatus("Failed");
-//							List<String> message = new ArrayList<String>();
-//							upload.getFailedItems().stream().forEach(x -> message.add(x.getMessage()));
-//							audit.setErrorMsg(message.get(0));
-//						}
-//						auditingRepository.save(audit);
-//					}
-//				} else if (audit.getOperation().equalsIgnoreCase("Download")) {
-//					HpcUserDownloadRequest download = downloadResults.stream()
-//							.filter(x -> audit.getTaskId().equals(x.getTaskId())).findAny().orElse(null);
-//					if (download != null) {
-//						audit.setCompletionTime((download != null && download.getCompleted() != null)
-//								? download.getCompleted().getTime()
-//								: null);
-//						if (download != null && download.getResult() != null
-//								&& download.getResult().value().equals("FAILED")) {
-//							List<String> message = new ArrayList<String>();
-//							download.getItems().stream().forEach(x -> message.add(x.getMessage()));
-//							audit.setStatus("Failed");
-//							audit.setErrorMsg(message.get(0));
-//
-//						} else if (download.getResult() != null && download.getResult().value().equals("COMPLETED")) {
-//							audit.setStatus("Completed");
-//						} else {
-//							audit.setStatus("In Progress");
-//						}
-//						auditingRepository.save(audit);
-//					}
-//				}
-//
-//			}
-//		}
-//	}
+	@Scheduled(cron = "${doe.scheduler.cron.auditing}")
+	public void updateAuditingService() throws DoeWebException {
+
+		log.info("auditing service scheduler");
+		String authToken = DoeClientUtil.getAuthenticationToken(writeAccessUserName, writeAccessUserPassword,
+				authenticateURL);
+
+		String serviceURL = queryServiceURL + "?page=" + 1 + "&totalCount=true";
+		HpcDownloadSummaryDTO downloads = DoeClientUtil.getDownloadSummary(authToken, serviceURL);
+
+		final MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
+		paramsMap.set("totalCount", Boolean.TRUE.toString());
+		HpcRegistrationSummaryDTO registrations = DoeClientUtil.getRegistrationSummary(authToken,
+				registrationServiceV2URL, paramsMap);
+
+		List<HpcUserDownloadRequest> downloadResults = new ArrayList<HpcUserDownloadRequest>();
+		if (downloads != null) {
+			downloadResults.addAll(downloads.getActiveTasks());
+			downloadResults.addAll(downloads.getCompletedTasks());
+		}
+
+		List<HpcBulkDataObjectRegistrationTaskDTO> uploadResults = new ArrayList<HpcBulkDataObjectRegistrationTaskDTO>();
+		if (registrations != null) {
+			uploadResults.addAll(registrations.getActiveTasks());
+			uploadResults.addAll(registrations.getCompletedTasks());
+		}
+
+		List<Auditing> auditingTaskIds = auditingService.getAllTaskIds();
+
+		if (CollectionUtils.isNotEmpty(auditingTaskIds)) {
+			for (Auditing audit : auditingTaskIds) {
+				if (audit.getOperation().equalsIgnoreCase("Upload")) {
+					HpcBulkDataObjectRegistrationTaskDTO upload = uploadResults.stream()
+							.filter(x -> audit.getTaskId().equals(x.getTaskId())).findAny().orElse(null);
+
+					if (upload != null) {
+						audit.setCompletionTime(
+								(upload != null && upload.getCompleted() != null) ? upload.getCompleted().getTime()
+										: null);
+						if (upload.getResult() == null) {
+							audit.setStatus("In progress");
+						} else if (Boolean.TRUE.equals(upload.getResult())) {
+							audit.setStatus("Completed");
+						} else if (Boolean.FALSE.equals(upload.getResult())) {
+							audit.setStatus("Failed");
+							List<String> message = new ArrayList<String>();
+							upload.getFailedItems().stream().forEach(x -> message.add(x.getMessage()));
+							audit.setErrorMsg(message.get(0));
+						}
+						auditingRepository.save(audit);
+					}
+				} else if (audit.getOperation().equalsIgnoreCase("Download")) {
+					HpcUserDownloadRequest download = downloadResults.stream()
+							.filter(x -> audit.getTaskId().equals(x.getTaskId())).findAny().orElse(null);
+					if (download != null) {
+						audit.setCompletionTime((download != null && download.getCompleted() != null)
+								? download.getCompleted().getTime()
+								: null);
+						if (download != null && download.getResult() != null
+								&& download.getResult().value().equals("FAILED")) {
+							List<String> message = new ArrayList<String>();
+							download.getItems().stream().forEach(x -> message.add(x.getMessage()));
+							audit.setStatus("Failed");
+							audit.setErrorMsg(message.get(0));
+
+						} else if (download.getResult() != null && download.getResult().value().equals("COMPLETED")) {
+							audit.setStatus("Completed");
+						} else {
+							audit.setStatus("In Progress");
+						}
+						auditingRepository.save(audit);
+					}
+				}
+
+			}
+		}
+	}
 
 	@Scheduled(cron = "${doe.scheduler.cron.infer}")
 	public void performInferencing() throws DoeWebException, MalformedURLException {
