999db7538e599449c181c5241ed11ed2d75d3858
Mounica Ganta <mounica.ganta@gmail.com>
2022-01-27 11:45:43 -0500

    clean up code

--- src/main/resources/static/js/controller/common.js
+++ src/main/resources/static/js/controller/common.js
@@ -376,8 +376,6 @@ $(document)
 
 					$('body').on('click',function(e) {
 						
-						$("[data-toggle=popover]").popover({html:true});
-						
 						 $('[data-toggle=popover]').each(function() {
 							 if (!$(this).is(e.target) && $(this).has(e.target).length === 0
 								   && $('.popover').has(e.target).length === 0) {
@@ -577,98 +575,6 @@ $(document).on('click', '.sharableLink', function() {
 
 });
 
-$(document).on('change', '#uploadTestInferFile', function() {
-	var $this = $(this);
-	var filename = $this.val().replace(/^C:\\fakepath\\/, "")
-	var value = $("#testInputPath").val() + "/" + filename;
-	$("#registerFileModal").find("#testInputPath").val(value);
-});
-
-$(document).on('click', '#openInferModal', function() {
-	$("#dataSetTable tr").each(function() {
-		var len = $(this).find('td:first input[type=checkbox]').length;
-		if (len > 0) {
-			var value = $(this).find('td:first input[type=checkbox]').attr('id');
-			if (value && value.indexOf('.h5') != -1) {
-				$("#registerFileModal").find("#modelPath").val(value);
-			}
-		}
-	});
-
-	var modelFile = $("#registerFileModal").find("#modelPath").val();
-	$("#registerFileModal").find("#testInputPath").val($("#selectedAssetPath").text());
-	if(modelFile.indexOf('mt_cnn') != -1 || modelFile.indexOf('mt-cnn') != -1) {
-		var title="Upload GDC manifest or <br/> pathology report. <br/>For more details, refer to the<br/><a target='_blank' href='https://wiki.nci.nih.gov/x/cQh2H'> user guide</a>.";
-		var outputTitle = "This is optional. Upload file to evaluate the model. <br/>For more details, refer to the<br/><a target='_blank' href='https://wiki.nci.nih.gov/x/cQh2H'> user guide</a>.";
-	} else {
-		var title="Upload GDC manifest or <br/> FPKM-UQ file. <br/>For more details, refer to the<br/><a target='_blank' href='https://wiki.nci.nih.gov/x/bwh2H'> user guide</a>.";
-		var outputTitle = "This is optional. Upload file to evaluate the model. <br/>For more details, refer to the<br/><a target='_blank' href='https://wiki.nci.nih.gov/x/bwh2H'> user guide</a>.";
-	}
-	
-	$("#registerFileModal").find("#tooltipHtml").html('<i class="fas fa-question-circle infoTooltip"'+
-			'data-toggle="popover" data-content="'+title+'"></i>');
-	
-	$("#registerFileModal").find("#outputTooltipHtml").html('<i class="fas fa-question-circle infoTooltip"'+
-			'data-toggle="popover" data-content="'+outputTitle+'"></i>');
-	
-	$(".performInferencingError").hide();
-	$(".performInferMsgError").html("");
-	$("input[name=uploadFrom]").prop("checked",false);
-	$("#uploadTestInferFile").val("");
-	$("#uploadTestOutputFile").val("")
-	$("#registerFileModal").modal({show: true, backdrop: 'static', keyboard: false});
-
-});
-
-$(document).on('click', '#btnRegisterFile', function() {
-
-	var form = $('#performInferForm')[0];
-	var data = new FormData(form);
-	var file = $("#uploadTestInferFile").val();
-	var uploadType = $('input[name=uploadFrom]:checked').val();
-	
-	if(!file) {
-		$(".performInferencingError").show();
-		$(".performInferMsgError").html("Upload file to generate predictions.")
-	}
-	
-	if(!uploadType) {
-		$(".performInferencingError").show();
-		$(".performInferMsgError").html("Choose upload data from type.")
-	}
-	
-	if (file && uploadType) {
-		$.ajax({
-			type : "POST",
-			enctype : "multipart/form-data",
-			url : "/performInferencing",
-			data : data,
-			processData : false,
-			contentType : false,
-			beforeSend : function() {
-				$("#spinner").show();
-				$("#dimmer").show();
-			},
-			success : function(msg) {
-				$("#spinner").hide();
-				$("#dimmer").hide();
-				$("#registerFileModal").modal('hide');
-				bootbox.dialog({
-					message : msg
-				});
-
-			},
-			error : function(e) {
-				$("#spinner").hide();
-				$("#dimmer").hide();
-				console.log('ERROR: ', e);
-				bootbox.dialog({
-					message : e
-				});
-			}
-		});
-	}
-});
 
 $(document).on('click', '.clearMetadata', function() {
 	$(this).parent().find("input[type='text']").val("");
