a14647f1ec4170bb21ad090c7b9abedc57f0a7e4
Mounica Ganta <mounica.ganta@gmail.com>
2022-05-31 15:31:07 -0400

    scripts

---
+++ scripts/schema/prediction_access_t.sql
@@ -0,0 +1,26 @@
+-- auto-generated definition
+create table PREDICTION_ACCESS_T
+(
+    ID              NUMBER generated as identity,
+    COLLECTION_ID   NUMBER         not null,
+    COLLECTION_PATH VARCHAR2(1000) not null,
+    USER_ID         NUMBER
+        constraint PREDICTION_ACCESS_T_USER_T_ID_FK
+            references USER_T,
+    GROUP_ID        NUMBER
+        constraint PREDICTION_ACCESS_T_GROUP_T_ID_FK
+            references GROUP_T,
+    CREATED_DATE    DATE,
+    IS_PUBLIC       CHAR
+)
+/
+
+create unique index PREDICTION_ACCESS_T_ID_UINDEX
+    on PREDICTION_ACCESS_T (ID)
+/
+
+alter table PREDICTION_ACCESS_T
+    add constraint PREDICTION_ACCESS_T_PK
+        primary key (ID)
+/
+
