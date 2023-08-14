00bc30442f475d52eda0132f671aa847f106c039
Mounica Ganta <mounica.ganta@gmail.com>
2023-08-14 12:57:25 -0400

    MODAC-317;

--- src/main/resources/static/js/controller/footer.js
+++ src/main/resources/static/js/controller/footer.js
@@ -3,31 +3,17 @@ $(document).ready(function () {
  //onclick on sign up for email updates sign up button
 
  	
- 	$('#myForm').submit(function(event) {
+ 	$('#signup').submit(function(event) {
 	    
 	   event.preventDefault(); // Prevent the default form submission
 	   var emailAddress = $("#signUpEmailAddress").val();
-		
-	   var valid = false;
-	   var filter = new RegExp('^[_A-Za-z0-9-!#$%&\'*+/=?^_`{|}~]+(\\.[_A-Za-z0-9-!#$%&\'*+/=?^_`{|}~]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$');
-       if(emailAddress != null && emailAddress != "") {
-	    	if (filter.test(emailAddress)) {
-		        valid = true;
-		    }
-   		}
-   		
-	    if (emailAddress === '') {
-	      $('.errorBanner').text('Please fill out this field');
-	    } else if(valid == false) {
-	      $('.errorBanner').text('Please enter a valid email address');
-	    } else {
-	      $('.errorBanner').text('');
-	       	var params = {emailAddress : emailAddress};
+
+	   var params = {emailAddress : emailAddress};
 	       	
-	       	invokeAjax('/emailNotifications/subscribe', 'POST', params, postSuccessEmailUpdatesFunction,
-								postFailureEmailUpdatesFunction,
-								'application/x-www-form-urlencoded; charset=UTF-8', 'text');
-		}
+       invokeAjax('/emailNotifications/subscribe', 'POST', params, postSuccessEmailUpdatesFunction,
+						postFailureEmailUpdatesFunction,
+						'application/x-www-form-urlencoded; charset=UTF-8', 'text');
+		
 	
  	});
 });
