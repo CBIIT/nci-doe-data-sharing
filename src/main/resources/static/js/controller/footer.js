544c7482f3c7aef9f8b811b8e87735792a3d3710
Mounica Ganta <mounica.ganta@gmail.com>
2023-08-18 17:01:23 -0400

    MODAC-317;

--- src/main/resources/static/js/controller/footer.js
+++ src/main/resources/static/js/controller/footer.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
 
- //onclick on sign up for email updates sign up button
+ var signUpEmailInput = $("input[name=signUpEmailAddress]");
 
+       signUpEmailInput.on("input", function () {
+       
+	       var email= $("#signUpEmailAddress").val();
+	       var filter = new RegExp('^[_A-Za-z0-9-!#$%&\'*+/=?^_`{|}~]+(\\.[_A-Za-z0-9-!#$%&\'*+/=?^_`{|}~]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$');
+	    		
+		  	if (!filter.test(email) || email == null || email == "") {
+		  		this.setCustomValidity("Enter a valid email.");		        
+		    }  else {
+	            this.setCustomValidity("");
+	        }
+        });
+ 
+  //onclick on sign up for email updates sign up button
  	
  	$('#signup').submit(function(event) {
 	    
@@ -9,10 +22,21 @@ $(document).ready(function () {
 	   var emailAddress = $("#signUpEmailAddress").val();
 
 	   var params = {emailAddress : emailAddress};
+	   
+	   var filter = new RegExp('^[_A-Za-z0-9-!#$%&\'*+/=?^_`{|}~]+(\\.[_A-Za-z0-9-!#$%&\'*+/=?^_`{|}~]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$');
+	   var signUpEmailInput = $("input[name=signUpEmailAddress]");	
+	   	
+		  	if (!filter.test(emailAddress) || emailAddress == null || emailAddress == "") {
+		  		signUpEmailInput[0].setCustomValidity("Enter a valid email.");		        
+		    }  else {
+		    	signUpEmailInput[0].setCustomValidity("");
+		    	
+    	       invokeAjax('/emailNotifications/subscribe', 'POST', params, postSuccessEmailUpdatesFunction,
+				postFailureEmailUpdatesFunction,
+				'application/x-www-form-urlencoded; charset=UTF-8', 'text');
+	        }
 	       	
-       invokeAjax('/emailNotifications/subscribe', 'POST', params, postSuccessEmailUpdatesFunction,
-						postFailureEmailUpdatesFunction,
-						'application/x-www-form-urlencoded; charset=UTF-8', 'text');
+
 		
 	
  	});
