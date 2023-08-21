319a4ad4ebf2d9736d9e0db4c53acdd2fc63c15e
Mounica Ganta <mounica.ganta@gmail.com>
2023-08-21 18:42:27 -0400

    setting json mapper set fail on unknown properties

--- src/main/java/gov/nih/nci/doe/web/controller/RestAPICommonController.java
+++ src/main/java/gov/nih/nci/doe/web/controller/RestAPICommonController.java
@@ -20,6 +20,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MappingJsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -972,6 +973,7 @@ public class RestAPICommonController extends AbstractDoeController {
 			ObjectMapper mapper = new ObjectMapper();
 			mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 			mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 			return new ResponseEntity<>(mapper.writeValueAsString(parser.readValueAs(HpcCollectionListDTO.class)),
 					HttpStatus.OK);
 
