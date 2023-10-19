4f5e3a3f3f7b92d32556d5f2606462c794797cf2
Mounica Ganta <mounica.ganta@gmail.com>
2023-10-19 11:28:40 -0400

    adding Db scripts for this release

--- scripts/schema/lookup.sql
+++ scripts/schema/lookup.sql
@@ -1,18 +1,23 @@
 -- auto-generated definition
-create table LOOKUP_T
+create table NCI_DOE_DB.LOOKUP_T
 (
-    ID                      NUMBER not null
+    ID                        NUMBER not null
         constraint LOOKUP_PKEY
             primary key,
-    LEVEL_NAME              VARCHAR2(255),
-    ATTR_NAME               VARCHAR2(255),
-    DISPLAY_NAME            VARCHAR2(500),
-    CREATED_BY              VARCHAR2(255),
-    CREATED_DATE            DATE,
-    DISPLAY_ORDER           NUMBER,
-    SEARCH_CRITERIA_DISPLAY CHAR,
-    SEARCH_RESULTS_DISPLAY  CHAR,
-    IS_EDITABLE             CHAR
+    LEVEL_NAME                VARCHAR2(255),
+    ATTR_NAME                 VARCHAR2(255),
+    DISPLAY_NAME              VARCHAR2(500),
+    CREATED_BY                VARCHAR2(255),
+    CREATED_DATE              DATE,
+    DISPLAY_ORDER             NUMBER,
+    SEARCH_CRITERIA_DISPLAY   CHAR,
+    IS_EDITABLE               CHAR,
+    IS_VISIBLE                CHAR,
+    IS_VISIBLE_ON_UPLOAD_PAGE CHAR
 )
 /
+
+comment on column NCI_DOE_DB.LOOKUP_T.IS_VISIBLE is 'This column is only for display purpose on asset details page. If value is ''N'', do not display on asset details page. '
+/
+
 
