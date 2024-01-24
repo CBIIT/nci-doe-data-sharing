49847883aae3f86baf6b98af52ac442d39b5a887
Mounica Ganta <mounica.ganta@gmail.com>
2024-01-24 15:28:04 -0500

    clean up

--- src/test/java/gov/nih/nci/doe/web/DoeWebApplicationTests.java
+++ src/test/java/gov/nih/nci/doe/web/DoeWebApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import gov.nih.nci.doe.web.DoeWebApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = DoeWebApplication.class)
