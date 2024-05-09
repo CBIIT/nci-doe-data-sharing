a6706d5c9c083a0a606e50fdbaccaf4f5a7d0994
Mounica Ganta <mounica.ganta@gmail.com>
2024-05-09 14:27:15 -0400

    sql scripts

--- scripts/schema/taskmanager.sql
+++ scripts/schema/taskmanager.sql
@@ -1,16 +1,18 @@
--- auto-generated definition
 create table TASK_MANAGER_T
 (
-    ID            NUMBER not null
+    ID        NUMBER not null
         constraint TASK_MANAGER_T_PK
             primary key,
-    TASK_ID       VARCHAR2(500),
-    TASK_NAME     VARCHAR2(500),
-    TASK_TYPE     VARCHAR2(255),
-    TASK_DATE     DATE,
-    USER_ID       VARCHAR2(255),
-    DOWNLOAD_TYPE VARCHAR2(255)
+    TASK_ID   VARCHAR2(500),
+    TASK_NAME VARCHAR2(500),
+    TASK_TYPE VARCHAR2(255),
+    TASK_DATE DATE,
+    USER_ID   VARCHAR2(255),
+    TYPE      VARCHAR2(255),
+    PATH      VARCHAR2(500)
 )
 /
+
+
 
 GRANT INSERT,UPDATE,DELETE,SELECT ON TASK_MANAGER_T TO MODAC_APP_USER;
