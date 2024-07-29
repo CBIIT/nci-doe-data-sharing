653b1953e175fcb05365da0c4b6a2b91adfd02ed
Aj <doddapaneni.ajay@gmail.com>
2024-07-29 18:16:55 -0500

    upload asset code review feedback

--- src/main/resources/static/js/controller/upload/upload_bulkassets.js
+++ src/main/resources/static/js/controller/upload/upload_bulkassets.js
@@ -1,23 +1,22 @@
 $(document).ready(
-		function() {
+	function () {
 
-			$("#assetSelectionGlobusButton").click(function(e) {
-				var d = {};
-				d.programPath = $("#programList").val();
-				d.studyPath = $("#studyList").val();
-				d.uploadType = "assetBulkUpload";
+		$("#assetSelectionGlobusButton").click(function (e) {
+			var d = {};
+			d.programPath = $("#programList").val();
+			d.studyPath = $("#studyList").val();
+			d.uploadType = "assetBulkUpload";
 
-				invokeAjax('/upload', 'GET', d, postUploadGlobusFunction, postFailureFunction, null, 'text');
-			});
+			invokeAjax('/upload', 'GET', d, postUploadGlobusFunction, postFailureFunction, null, 'text');
+		});
 
-			$(document).ready(function() {
-				$('.simple-select2').select2();
-			});
 
-			$('#registerAssetSelect').on('change', function() {
-				var selectedOption = $("#registerAssetSelect option:selected").text();
-				console.log('selectedOption');
-				console.log(selectedOption);
+		$('#registerAssetSelect').on('change', function () {
+			var selectedOption = $("#registerAssetSelect option:selected").text();
+
+			console.log('selectedOption');
+			console.log(selectedOption);
+			if (selectedOption !== 'Select Asset Type') {
 				$("#assetType").val(selectedOption);
 				$("#uploadSectionDiv").hide();
 				$("#uploadHeader").hide();
@@ -25,29 +24,30 @@ $(document).ready(
 				$("#registerCollectionForm").show()
 				createCollectionDiv('studyList');
 				$("#registerAssetPicker").show();
+			}
 
-			});
+		});
 
 
-			$('input[type=radio][name=registerAssetRadio]').change(function() {
-                if (this.value == 'Register Asset') {
-                    $('#registerAssetSelect').prop('disabled', false);
-                    $('#uploadAssetSelect').prop('disabled', true);
-                } else if (this.value == 'Upload Asset') {
-                    $('#registerAssetSelect').prop('disabled', true);
-                    $('#uploadAssetSelect').prop('disabled', false);
-                }
+		$('input[type=radio][name=registerAssetRadio]').change(function () {
+			if (this.value == 'Register Asset') {
+				$('#registerAssetSelect').prop('disabled', false);
+				$('#uploadAssetSelect').prop('disabled', true);
+			} else if (this.value == 'Upload Asset') {
+				$('#registerAssetSelect').prop('disabled', true);
+				$('#uploadAssetSelect').prop('disabled', false);
+			}
 
-            });
+		});
 
-});
+	});
 
 function retrieveAssetTypeDiv(data) {
 	if (data.value != 'Select') {
 		var params1 = {
-			collectionType : 'Asset',
-			controllerValue : data.value,
-			controllerAttribute : 'asset_type'
+			collectionType: 'Asset',
+			controllerValue: data.value,
+			controllerAttribute: 'asset_type'
 		};
 		invokeAjax('/addCollection', 'GET', params1, constructAssetTypeBulkDiv, null, null, null);
 	} else {
@@ -60,89 +60,89 @@ function constructAssetTypeBulkDiv(data, status) {
 	$("#addMetadataDiv").show();
 	$("#assetBulkMetadataTable tbody").html("");
 	$
-			.each(
-					data,
-					function(key, value) {
+		.each(
+			data,
+			function (key, value) {
 
-						var placeholderValue = value.mandatory == true ? 'Required' : "";
-						var infoHtml = "";
-						if(value.description) {
-						infoHtml = '<i class="fas fa-info-square" data-toggle="tooltip"'
-														+ 'data-placement="right" title="'
-														+ value.description
-														+ '"></i>';
+				var placeholderValue = value.mandatory == true ? 'Required' : "";
+				var infoHtml = "";
+				if (value.description) {
+					infoHtml = '<i class="fas fa-info-square" data-toggle="tooltip"'
+						+ 'data-placement="right" title="'
+						+ value.description
+						+ '"></i>';
+				}
+
+				if (value.validValues != null && value.attrName != 'asset_type' && value.isVisibleOnUplaodPage != false) {
+
+					$("#assetBulkMetadataTable tbody")
+						.append(
+							'<tr><td>'
+							+ value.displayName
+							+ '&nbsp;&nbsp;' + infoHtml + '</td><td>'
+							+ '<select class="simple-select2" is_mandatory="'
+							+ value.mandatory
+							+ '" style="width:99%;" onChange="onChangeForMetadata(registerBulkAssetForm, true,'
+							+ value.controllerAttribute + ',assetBulkMetadataTable, '
+							+ value.attrName + ');"' + 'id="' + value.attrName
+							+ '" name="zAttrStr_' + value.attrName + '" value="'
+							+ value.attrValue + '"></select></td></tr>');
+
+					var $select = $("#" + value.attrName);
+					if (value.attrValue) {
+						$select.append($('<option></option>').attr('value', value.attrValue).text(
+							value.attrValue));
+					} else if (!value.defaultValue) {
+						$select.append($('<option></option>').attr('value', 'Select').text('Select'));
+					}
+
+					for (var i = 0; i < value.validValues.length; i++) {
+						if ($select.find("option[value='" + value.validValues[i].key + "']").length == 0) {
+							$select.append($('<option></option>').attr('value', value.validValues[i].key).text(
+								value.validValues[i].value));
 						}
+					}
 
-						if (value.validValues != null && value.attrName != 'asset_type' && value.isVisibleOnUplaodPage != false) {
+					if (value.defaultValue) {
+						$select.select2().val(value.defaultValue);
+					} else {
+						$select.select2();
+					}
 
-							$("#assetBulkMetadataTable tbody")
-									.append(
-											'<tr><td>'
-													+ value.displayName
-													+ '&nbsp;&nbsp;' + infoHtml + '</td><td>'
-													+ '<select class="simple-select2" is_mandatory="'
-													+ value.mandatory
-													+ '" style="width:99%;" onChange="onChangeForMetadata(registerBulkAssetForm, true,'
-													+ value.controllerAttribute + ',assetBulkMetadataTable, '
-													+ value.attrName + ');"' + 'id="' + value.attrName
-													+ '" name="zAttrStr_' + value.attrName + '" value="'
-													+ value.attrValue + '"></select></td></tr>');
+				} else if (value.attrName && value.attrName != 'asset_type'
+					&& value.attrName != 'access_group' && value.isVisibleOnUplaodPage != false) {
+					if (value.attrName == 'description') {
+						$("#assetBulkMetadataTable tbody")
+							.append(
+								'<tr><td>'
+								+ value.displayName
+								+ '&nbsp;&nbsp;<i class="fas fa-info-square" data-toggle="tooltip"'
+								+ 'data-placement="right" title="Please note that this description will be applied to all Assets. You may edit the description of each Asset separately after it is uploaded."></i></td><td>'
+								+ '<textarea rows="4" placeholder ="Required" is_mandatory="'
+								+ value.mandatory
+								+ '" aria-label="value of meta data"  name="zAttrStr_'
+								+ value.attrName
+								+ '"'
+								+ 'style="width:100%;padding-left: 10px;font-family:inter_regular;"></textarea></td></tr>');
+					} else {
+						$("#assetBulkMetadataTable tbody").append(
+							'<tr><td>' + value.displayName
+							+ '&nbsp;&nbsp;' + infoHtml + '</td><td>' + '<input type="search" placeholder="'
+							+ placeholderValue + '" class="bulkAssetTextbox" is_mandatory="'
+							+ value.mandatory
+							+ '" aria-label="value of meta data"  name="zAttrStr_' + value.attrName
+							+ '"' + '></td></tr>');
+					}
+				}
 
-							var $select = $("#" + value.attrName);
-							if (value.attrValue) {
-								$select.append($('<option></option>').attr('value', value.attrValue).text(
-										value.attrValue));
-							} else if (!value.defaultValue) {
-								$select.append($('<option></option>').attr('value', 'Select').text('Select'));
-							}
-
-							for (var i = 0; i < value.validValues.length; i++) {
-							  if ($select.find("option[value='" + value.validValues[i].key + "']").length == 0) {
-								 $select.append($('<option></option>').attr('value', value.validValues[i].key).text(
-										value.validValues[i].value));
-							  }
-							}
-
-							if (value.defaultValue) {
-								$select.select2().val(value.defaultValue);
-							} else {
-								$select.select2();
-							}
-
-						} else if (value.attrName && value.attrName != 'asset_type'
-								 && value.attrName != 'access_group' && value.isVisibleOnUplaodPage != false) {
-							if (value.attrName == 'description') {
-								$("#assetBulkMetadataTable tbody")
-										.append(
-												'<tr><td>'
-														+ value.displayName
-														+ '&nbsp;&nbsp;<i class="fas fa-info-square" data-toggle="tooltip"'
-														+ 'data-placement="right" title="Please note that this description will be applied to all Assets. You may edit the description of each Asset separately after it is uploaded."></i></td><td>'
-														+ '<textarea rows="4" placeholder ="Required" is_mandatory="'
-														+ value.mandatory
-														+ '" aria-label="value of meta data"  name="zAttrStr_'
-														+ value.attrName
-														+ '"'
-														+ 'style="width:100%;padding-left: 10px;font-family:inter_regular;"></textarea></td></tr>');
-							} else {
-								$("#assetBulkMetadataTable tbody").append(
-										'<tr><td>' + value.displayName
-												+ '&nbsp;&nbsp;' + infoHtml + '</td><td>' + '<input type="search" placeholder="'
-												+ placeholderValue + '" class="bulkAssetTextbox" is_mandatory="'
-												+ value.mandatory
-												+ '" aria-label="value of meta data"  name="zAttrStr_' + value.attrName
-												+ '"' + '></td></tr>');
-							}
-						}
-
-					});
+			});
 }
 
 function registerBulkAssets() {
 
 	var studyPath = $("#studyPath").val();
-	if(!studyPath) {
-	 studyPath = $("#studyList").val();
+	if (!studyPath) {
+		studyPath = $("#studyList").val();
 	}
 	$("#registerBulkAssetForm").attr('bulkDatafilePath', studyPath);
 	var uploadType = $('input[name="assetSelection"]:checked').val();
@@ -157,7 +157,7 @@ function registerBulkAssets() {
 	var usermetaDataEntered = true;
 	var isFormBulkAssetUpload;
 
-	$('table#assetBulkMetadataTable input[type="search"]').each(function() {
+	$('table#assetBulkMetadataTable input[type="search"]').each(function () {
 		var name = $(this).val();
 		var ismandatory = $(this).attr('is_mandatory');
 		if (!name && ismandatory && ismandatory != "false") {
@@ -166,7 +166,7 @@ function registerBulkAssets() {
 		}
 	});
 
-	$("table#assetBulkMetadataTable").find("textarea").each(function() {
+	$("table#assetBulkMetadataTable").find("textarea").each(function () {
 		var ismandatory = $(this).attr('is_mandatory');
 		if (!$(this).val() && ismandatory && ismandatory != "false") {
 			isValidated = false;
@@ -174,7 +174,7 @@ function registerBulkAssets() {
 		}
 	});
 
-	$("table#assetBulkMetadataTable").find(".simple-select2").each(function() {
+	$("table#assetBulkMetadataTable").find(".simple-select2").each(function () {
 		var ismandatory = $(this).attr('is_mandatory');
 		var isMultiSelect = $(this).prop('multiple');
 		var name = $(this).val();
@@ -190,87 +190,87 @@ function registerBulkAssets() {
 		}
 	});
 
-    if (!usermetaDataEntered) {
-		    errorMsg = "Enter values for all required metadata.";
+	if (!usermetaDataEntered) {
+		errorMsg = "Enter values for all required metadata.";
 	}
-	
-    if(uploadType == 'globus') {
-   
-		   	if (!$("#assetGlobusEndpointId").length || !$("#assetGlobusEndpointPath").length) {
+
+	if (uploadType == 'globus') {
+
+		if (!$("#assetGlobusEndpointId").length || !$("#assetGlobusEndpointPath").length) {
+			isValidated = false;
+			errorMsg = "Select Assets from Globus.";
+
+		} else if (!$("#assetSelectedFolders").length) {
+			isValidated = false;
+			errorMsg = "Select Folders from Globus.";
+
+		} else if ($("#assetSelectedFiles").length) {
+			isValidated = false;
+			errorMsg = "Select folders only."
+
+		}
+
+	} else if (uploadType == 'S3') {
+
+		$('#S3DetailsDiv input[type="search"]').each(function () {
+			var ismandatory = $(this).attr('is_mandatory');
+			if ($(this).is(":visible") && !$(this).val() && ismandatory && ismandatory != "false") {
 				isValidated = false;
-				errorMsg = "Select Assets from Globus.";
-				
-			} else if (!$("#assetSelectedFolders").length) {
-				isValidated = false;
-				errorMsg = "Select Folders from Globus.";
-				
-			} else if ($("#assetSelectedFiles").length) {
-				isValidated = false;
-				errorMsg = "Select folders only."
-				
 			}
-	
-    } else if(uploadType == 'S3') {
-    
-	       $('#S3DetailsDiv input[type="search"]').each(function() {
-	            var ismandatory = $(this).attr('is_mandatory');
-				if ($(this).is(":visible") && !$(this).val() && ismandatory && ismandatory != "false") {
-					isValidated = false;
-				}
-			});
-			
-			if(!isValidated) {
-			    errorMsg = "Enter all the required fields."
-			}
-    }
-    
-    /* once all the above validation passes, do the following additional validations */
-    
-    if(isValidated) {
-	    if ($('input[name="assetUploadType"]').is(":visible") && $('input[name="assetUploadType"]:checked').length == 0) {
+		});
+
+		if (!isValidated) {
+			errorMsg = "Enter all the required fields."
+		}
+	}
+
+	/* once all the above validation passes, do the following additional validations */
+
+	if (isValidated) {
+		if ($('input[name="assetUploadType"]').is(":visible") && $('input[name="assetUploadType"]:checked').length == 0) {
 			isValidated = false;
 			errorMsg = "Select your choice of upload.";
-			
+
 		} else if ($("input[name='assetUploadType']:checked").val() == 'No' && $("#assetTypeSelect").val() == 'Select') {
 			isValidated = false;
 			errorMsg = "Select Asset Type.";
-			
+
 		} else if ($("#doeMetadataFile").is(":visible") && !$("#doeMetadataFile").val()) {
 			isValidated = false;
 			errorMsg = "Upload CSV Metadata file.";
-			
+
 		}
 	}
-	
-	if(errorMsg) {
-	   /* display the error message in a popup */
-	   bootbox.dialog({
-				message : errorMsg
-	   });
-	   
-    } else if (isValidated) {
-        
-        /* all the validations have passed, call the ajax to perform bulk asset upload */
-        
+
+	if (errorMsg) {
+		/* display the error message in a popup */
+		bootbox.dialog({
+			message: errorMsg
+		});
+
+	} else if (isValidated) {
+
+		/* all the validations have passed, call the ajax to perform bulk asset upload */
+
 		if ($("input[name='assetUploadType']:checked").val() == 'No') {
 			isFormBulkAssetUpload = true;
 		} else if ($("input[name='assetUploadType']:checked").val() == 'Yes') {
 			isFormBulkAssetUpload = false;
-		} else if(uploadType == 'globus' && $("#doeMetadataFile").is(":visible") && $("#doeMetadataFile").val()) {
-		    isFormBulkAssetUpload = false;
+		} else if (uploadType == 'globus' && $("#doeMetadataFile").is(":visible") && $("#doeMetadataFile").val()) {
+			isFormBulkAssetUpload = false;
 		}
 		$.ajax({
-			type : "POST",
-			enctype : "multipart/form-data",
-			url : "/addbulk?isFormBulkAssetUpload=" + isFormBulkAssetUpload,
-			data : data,
-			processData : false,
-			contentType : false,
-			beforeSend : function() {
+			type: "POST",
+			enctype: "multipart/form-data",
+			url: "/addbulk?isFormBulkAssetUpload=" + isFormBulkAssetUpload,
+			data: data,
+			processData: false,
+			contentType: false,
+			beforeSend: function () {
 				$("#spinner").show();
 				$("#dimmer").show();
 			},
-			success : function(msg) {
+			success: function (msg) {
 				$("#spinner").hide();
 				$("#dimmer").hide();
 				console.log('SUCCESS: ', msg);
@@ -279,25 +279,25 @@ function registerBulkAssets() {
 					msg = "Error in bulk upload";
 				}
 				bootbox.dialog({
-					message : msg,
-					onEscape : function() {
+					message: msg,
+					onEscape: function () {
 						location.replace("/addbulk");
 					}
 				});
 			},
-			error : function(e) {
+			error: function (e) {
 				$("#spinner").hide();
 				$("#dimmer").hide();
 				console.log('ERROR: ', e);
 				var error = "";
-				if(e && e.responseText) {
-				   error = e.responseText
+				if (e && e.responseText) {
+					error = e.responseText
 				} else {
-				   error = e;
+					error = e;
 				}
 				bootbox.dialog({
-					message : error,
-					onEscape : function() {
+					message: error,
+					onEscape: function () {
 						location.replace("/addbulk");
 					}
 				});
@@ -322,50 +322,50 @@ function displayAssetTpeSelection(data) {
 
 function displayBulkAssetSelection(data) {
 
-  if(data == 'Globus') {
-      $("#assetUploadDiv").addClass('show');
-      $("#globusDetailsDiv").show();
-      $("#S3DetailsDiv").hide();
-      var bulkUploadCollection = $("#bulkUploadCollection").val();
-      var folderLength = $("#assetSelectedFolders ul li").length;
-      resetAssetBulkUploadChoiceOptions();
-      if(!bulkUploadCollection) {
-         $("#bulkAssetOptionsDiv").hide();
-      } else  {
-      	 $("#bulkAssetOptionsDiv").show();
-      	 if(folderLength && folderLength > 1) {
-      	 
-      	      $("#multipleFoldersUploadDiv").hide();
-      	      $("#uploadCsvFile").show();
-      	      $("#formAssetSelection").hide();
-      	      $("#addMetadataDiv").hide();
-      	 } else  {
-      	      $("#multipleFoldersUploadDiv").show();
-      	      $("#uploadCsvFile").hide();
-      	 }
-      	
-      }
-  } else if(data == 'S3') {
-      $("#globusDetailsDiv").hide();
- 	  $("#assetUploadDiv").addClass('show');
- 	  $("#registerBulkAssets").prop("disabled", false);
- 	  $("#S3DetailsDiv").show();
- 	  $("#bulkAssetOptionsDiv").show();
- 	  $("#uploadCsvFile").hide();
- 	  $("#multipleFoldersUploadDiv").show();
- 	  resetAssetBulkUploadChoiceOptions();
- 	  
-  } else if(data =='emptyAsset') {
-  	  $("#assetUploadDiv").removeClass('show');
-  }
+	if (data == 'Globus') {
+		$("#assetUploadDiv").addClass('show');
+		$("#globusDetailsDiv").show();
+		$("#S3DetailsDiv").hide();
+		var bulkUploadCollection = $("#bulkUploadCollection").val();
+		var folderLength = $("#assetSelectedFolders ul li").length;
+		resetAssetBulkUploadChoiceOptions();
+		if (!bulkUploadCollection) {
+			$("#bulkAssetOptionsDiv").hide();
+		} else {
+			$("#bulkAssetOptionsDiv").show();
+			if (folderLength && folderLength > 1) {
+
+				$("#multipleFoldersUploadDiv").hide();
+				$("#uploadCsvFile").show();
+				$("#formAssetSelection").hide();
+				$("#addMetadataDiv").hide();
+			} else {
+				$("#multipleFoldersUploadDiv").show();
+				$("#uploadCsvFile").hide();
+			}
+
+		}
+	} else if (data == 'S3') {
+		$("#globusDetailsDiv").hide();
+		$("#assetUploadDiv").addClass('show');
+		$("#registerBulkAssets").prop("disabled", false);
+		$("#S3DetailsDiv").show();
+		$("#bulkAssetOptionsDiv").show();
+		$("#uploadCsvFile").hide();
+		$("#multipleFoldersUploadDiv").show();
+		resetAssetBulkUploadChoiceOptions();
+
+	} else if (data == 'emptyAsset') {
+		$("#assetUploadDiv").removeClass('show');
+	}
 }
 
 function resetAssetBulkUploadChoiceOptions() {
 
-	 $('input[name="assetUploadType"]').prop('checked', false);
-	 $("#uploadCsvFile").hide();
-	 $("#formAssetSelection").hide();
-	 $("#assetTypeSelect").val("Select").trigger("change");
+	$('input[name="assetUploadType"]').prop('checked', false);
+	$("#uploadCsvFile").hide();
+	$("#formAssetSelection").hide();
+	$("#assetTypeSelect").val("Select").trigger("change");
 }
 
 
@@ -373,6 +373,20 @@ function displayEmptyAssetScreen(data) {
 	$("#uploadSectionDiv").hide();
 	$("#uploadHeader").hide();
 	$("#registerCollectionForm").hide();
-	createCollectionDiv('studyList');	
+	createCollectionDiv('studyList');
 	$("#registerAssetPicker").show();
+
+	// Reset registerAssetSelect radio buttons
+	$('input[type=radio][name=registerAssetRadio]').prop('checked', false);
+
+	// Reset registerAssetSelect selection 
+	$('#registerAssetSelect').prop('selectedIndex', 0);
+	$('#registerAssetSelect').prop('disabled', true);
+	$('#registerAssetSelect').val('').trigger('change');
+
+	// Reset uploadAssetSelect selection 
+	$('#uploadAssetSelect').prop('selectedIndex', 0);
+	$('#uploadAssetSelect').prop('disabled', true);
+	$('#uploadAssetSelect').val('').trigger('change');
+
 }
